1b660d56c802111db5204ab58e0f99ac254f7705
AJClassic2014 <auguestconverse2@gmail.com>
2018-10-30 07:55:45 +0800

    add keys to mile range list

--- app/components/RangeList.jsx
+++ app/components/RangeList.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const RangeList = ({ models }) => (
   <div className="state">
     <ul>
-      {models.map((model) => <li><div className={
+      {models.map((model,i) => <li><div className={
         `state-img state-img-${model.name}`
       }></div><span id={
         `model_${model.name}`
-      }>{model.miles}</span></li>)}
+      } key={i}>{model.miles}</span></li>)}
     </ul>
   </div>
 );
