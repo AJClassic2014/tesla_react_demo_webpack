5a04bc28305dfb1f0527e16d3021412f704d1b21
AJClassic2014 <auguestconverse2@gmail.com>
2019-02-05 11:58:49 +0800

    replaced index.css by sass generated compressed css

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/App.jsx';
-import './assets/styles/index.css';
+import './assets/styles/index_compressed.css';
 import model_60 from './assets/json/model_60.json';
 import model_60D from './assets/json/model_60D.json';
 import model_75 from './assets/json/model_75.json';
